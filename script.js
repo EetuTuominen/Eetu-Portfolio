1ccf09d703fc1e66ec0cefd9304fceb3cada70fb
EetuTuominen <eetu.tuominen@edu.salpaus.fi>
2020-11-23 16:36:19 +0000

    2020.23.11

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("lightmode");
+}
+function myyFunction() {
+   var element = document.body;
+   element.classList.toggle("rainbow");
+   }

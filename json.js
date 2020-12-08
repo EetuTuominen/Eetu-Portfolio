24e78c7f9d4a7f807104e9955b73a436170f0638
EetuTuominen <eetu.tuominen@edu.salpaus.fi>
2020-12-08 10:22:31 +0000

    2020.12.8

--- json.js
+++ json.js
@@ -19,20 +19,25 @@ var getJSON = function(url, callback) {
 };
 
 getJSON
-('https://apify.com/covid-19', function(err, data) {
-  let taulukko = `<table width="100%" style="border=1px solid black">`
+('https://api.apify.com/v2/key-value-stores/jEFt5tgCTMfjJpLD3/records/LATEST?disableRedirect=true', function(err, data) {
+let taulukko = `<table width="100%" style="border=1px solid black">`
 
-const historia = data.map(function(paiva)){
-taulukko = taulukko + `<tr><td>${paiva.confirmed.Cases}</td></tr>`;
-
+const historia = data.map(function(paiva){
+    taulukko = taulukko + `<tr><td>${paiva.confirmedCases}</td></tr>`;
+ if(paiva.confirmedCases != undefined){
+ }
+   else if(paiva.testedCases != undefined){
+   }
+else{
+  taulukko = taulukko + `<tr><td>Ei dataa</td></tr>`;
 };
-
 taulukko = taulukko + `</table>`;
 });
+document.body.innerHTML = taulukko
 
     if (err != null) {
         console.error(err);
     } else {
-      console.log(´${data.infected}´);
+      console.log(´${paiva.infected}´);
     }
 });

62355741ded04624d278ef6572e0137f344ed2bd
EetuTuominen <eetu.tuominen@edu.salpaus.fi>
2020-12-08 08:21:01 +0000

    2020.12.8

--- json.js
+++ json.js
@@ -20,6 +20,15 @@ var getJSON = function(url, callback) {
 
 getJSON
 ('https://apify.com/covid-19', function(err, data) {
+  let taulukko = `<table width="100%" style="border=1px solid black">`
+
+const historia = data.map(function(paiva)){
+taulukko = taulukko + `<tr><td>${paiva.confirmed.Cases}</td></tr>`;
+
+};
+
+taulukko = taulukko + `</table>`;
+});
 
     if (err != null) {
         console.error(err);
